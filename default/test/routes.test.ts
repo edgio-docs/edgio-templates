b3349643bdb2e1d17a75da5fcec282b997cb851d
Tristan Lee <tristanlee85@gmail.com>
2021-06-22 11:55:06 -0400

    Adjust tests for default template

--- default/test/routes.test.ts
+++ default/test/routes.test.ts
@@ -7,20 +7,23 @@ describe('routes', () => {
     it('should cache the homepage at the edge and service worker, but not the browser', async () => {
       const result = await runRoute(routes, '/')
       expect(result).toBeCachedByTheEdgeFor(3600)
-      expect(result).toBeServedStale()
+      expect(result).not.toBeServedStale()
       expect(result).not.toBeCachedByTheBrowser()
       expect(result).toBeCachedByTheServiceWorkerFor(3600)
     })
 
     it('should return content from the origin', async () => {
-      const result = await runRoute(routes, '/')
       const body = `<!DOCTYPE html><html><body>Test</body></html>`
 
       nock(`https://${backendHost('origin')}`)
         .get('/')
         .reply(200, body)
 
-      expect(result).toHaveBody(body)
+      const result = await runRoute(routes, '/')
+
+      // Our route transforms the response to inject a script so only match
+      // the HTML body
+      expect(result).toHaveBody(/<!DOCTYPE html><html>.*<body>Test<\/body><\/html>/)
     })
   })
 })
